08b0c7bd83947dc19ecbbb281a1c430dd6a9180b
Iankill <killiancazin@gmail.com>
2014-04-23 19:58:56 +0100

    Access token and Same origin policy fix

--- js/video.js
+++ js/video.js
@@ -1,5 +1,7 @@
-var nextPageToken = undefined;
+var nextPageToken = "";
+var accessToken = "AIzaSyC4ai2cX-VOhGZXI_I6Hs19YkyY60_r6Yg";
 var q;
+
 
 function postVideos(data){
 	$("#videoWrapper").empty();
@@ -13,12 +15,13 @@ function postVideos(data){
 
 function searchVideo(q) {
 	q = q;
-	requestUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&videoEmbeddable=true&key=AIzaSyAsmDFjbvQQe8TktvikOwLzyn56gD9SOlQ&q="+q+nextPageToken;
+	requestUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&videoEmbeddable=true&key="+accessToken+"&q="+q+nextPageToken;
 	$.ajax({
 		type: "GET",
-		dataType: 'jsonp',
+		dataType: 'json',
 		url: requestUrl,
 		success: function(data) {
+			console.log(data);
 			nextPageToken = "&pageToken="+data.nextPageToken;
 			postVideos(data.items);
 		}
